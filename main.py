57083cd205bd8aad9e3e753f8a17147c7ce2c80d
t-wallner <thomaswallner@thomaswallnermacbook.mynet>
2019-04-20 18:29:20 +0100

    Pulling JPM stock data into df

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import datetime as dt
 import matplotlib.pyplot as plt
-from matplotlin import style
+from matplotlib import style
+import pandas as pd 
+import pandas_datareader.data as web
+
+style.use("ggplot")
+
+start = dt.datetime(2000,1,1)
+end = dt.datetime(2019,4,1)
+
+df = web.DataReader("JPM","yahoo", start, end)
+print(df.head(5))
